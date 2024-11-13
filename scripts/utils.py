fbac175116e51d0abd791cb8462f7061be0e8573
jung0228 <junghw3333@gmail.com>
2024-11-13 10:25:31 +0000

    feat: add utils.py

---
+++ scripts/utils.py
@@ -0,0 +1,154 @@
+import datetime
+import torch
+import numpy as np
+import random
+import torch.nn.functional as F
+import tqdm
+
+CLASSES = [
+    'finger-1', 'finger-2', 'finger-3', 'finger-4', 'finger-5',
+    'finger-6', 'finger-7', 'finger-8', 'finger-9', 'finger-10',
+    'finger-11', 'finger-12', 'finger-13', 'finger-14', 'finger-15',
+    'finger-16', 'finger-17', 'finger-18', 'finger-19', 'Trapezium',
+    'Trapezoid', 'Capitate', 'Hamate', 'Scaphoid', 'Lunate',
+    'Triquetrum', 'Pisiform', 'Radius', 'Ulna',
+]
+
+def dice_coef(y_true, y_pred):
+    """
+    Calculate the Dice coefficient (a measure of overlap) between true and predicted labels.
+
+    Args:
+        y_true (Tensor): Ground truth labels, shape [batch_size, num_classes, height, width]
+        y_pred (Tensor): Predicted labels, shape [batch_size, num_classes, height, width]
+
+    Returns:
+        Tensor: Dice coefficient for each class in the batch, shape [batch_size]
+    """
+    y_true_f = y_true.flatten(2)
+    y_pred_f = y_pred.flatten(2)
+    intersection = torch.sum(y_true_f * y_pred_f, -1)
+
+    eps = 0.0001
+    return (2. * intersection + eps) / (torch.sum(y_true_f, -1) + torch.sum(y_pred_f, -1) + eps)
+
+
+def save_model(model, output_path, file_name='best_model.pt'):
+    """
+    Save the trained model to a file.
+
+    Args:
+        model (torch.nn.Module): The model to be saved.
+        output_path (str): Directory path where the model will be saved.
+        file_name (str, optional): Name of the saved model file. Defaults to 'best_model.pt'.
+    """
+    torch.save(model.state_dict(), f"{output_path}/{file_name}")
+    print(f"Model saved to {output_path}/{file_name}")
+
+
+def set_seed(seed=21):
+    """
+    Set the seed for reproducibility across various libraries (PyTorch, NumPy, and Python's random module).
+
+    Args:
+        seed (int, optional): The seed value to set. Defaults to 42.
+    """
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)  # if using multi-GPU
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+    np.random.seed(seed)
+    random.seed(seed)
+    print(f"Random seed set to {seed}")
+
+def validation(epoch, model, data_loader, criterion, thr=0.5):
+    print(f'Start validation #{epoch:2d}')
+    model.cuda()
+    model.eval()
+
+    dices = []
+    with torch.no_grad():
+        n_class = len(CLASSES)
+        total_loss = 0
+        cnt = 0
+
+        for step, (images, masks) in tqdm(enumerate(data_loader), total=len(data_loader)):
+            images, masks = images.cuda(), masks.cuda()
+
+            outputs = model(images)
+
+            output_h, output_w = outputs.size(-2), outputs.size(-1)
+            mask_h, mask_w = masks.size(-2), masks.size(-1)
+
+            # restore original size
+            if output_h != mask_h or output_w != mask_w:
+                outputs = F.interpolate(outputs, size=(mask_h, mask_w), mode="bilinear")
+
+            loss = criterion(outputs, masks)
+            total_loss += loss
+            cnt += 1
+
+            outputs = torch.sigmoid(outputs)
+            outputs = (outputs > thr).detach().cpu()
+            masks = masks.detach().cpu()
+
+            dice = dice_coef(outputs, masks)
+            dices.append(dice)
+
+    dices = torch.cat(dices, 0)
+    dices_per_class = torch.mean(dices, 0)
+    dice_str = [
+        f"{c:<12}: {d.item():.4f}"
+        for c, d in zip(CLASSES, dices_per_class)
+    ]
+    dice_str = "\n".join(dice_str)
+    print(dice_str)
+
+    avg_dice = torch.mean(dices_per_class).item()
+
+    return avg_dice
+
+def train(model, data_loader, val_loader, criterion, optimizer, 
+          NUM_EPOCHS=20, VAL_EVERY=10, SAVED_DIR="../ckeckpoints/"):
+    print(f'Start training..')
+    model.cuda()
+
+    n_class = len(CLASSES)
+    best_dice = 0.
+
+    for epoch in range(NUM_EPOCHS):
+        model.train()
+
+        for step, (images, masks) in enumerate(data_loader):
+            # gpu 연산을 위해 device 할당
+            images, masks = images.cuda(), masks.cuda()
+
+
+            # inference
+            outputs = model(images)
+
+            # loss 계산
+            loss = criterion(outputs, masks)
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            # step 주기에 따른 loss 출력
+            if (step + 1) % 25 == 0:
+                print(
+                    f'{datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")} | '
+                    f'Epoch [{epoch+1}/{NUM_EPOCHS}], '
+                    f'Step [{step+1}/{len(train_loader)}], '
+                    f'Loss: {round(loss.item(),4)}'
+                )
+
+        # validation 주기에 따른 loss 출력 및 best model 저장
+        if (epoch + 1) % VAL_EVERY == 0:
+            dice = validation(epoch + 1, model, val_loader, criterion)
+
+            if best_dice < dice:
+                print(f"Best performance at epoch: {epoch + 1}, {best_dice:.4f} -> {dice:.4f}")
+                print(f"Save model in {SAVED_DIR}")
+                best_dice = dice
+                save_model(model)
