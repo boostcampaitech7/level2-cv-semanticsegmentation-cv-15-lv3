a47037e93fe717be348b42c0d62b4677fd140dcd
jung0228 <junghw3333@gmail.com>
2024-11-13 10:26:28 +0000

    feat: model.py

---
+++ scripts/models.py
@@ -0,0 +1,88 @@
+# unetplusplus.py
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class ConvBlockNested(nn.Module):
+    def __init__(self, in_ch, mid_ch, out_ch):
+        super(ConvBlockNested, self).__init__()
+        self.activation = nn.ReLU(inplace=True)
+        self.conv1 = nn.Conv2d(in_ch, mid_ch, kernel_size=3, padding=1, bias=True)
+        self.bn1 = nn.BatchNorm2d(mid_ch)
+        self.conv2 = nn.Conv2d(mid_ch, out_ch, kernel_size=3, padding=1, bias=True)
+        self.bn2 = nn.BatchNorm2d(out_ch)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.activation(x)
+        x = self.conv2(x)
+        x = self.bn2(x)
+        return self.activation(x)
+
+class UNetPlusPlus(nn.Module):
+    def __init__(self, in_ch=3, out_ch=1, n1=64, height=512, width=512, supervision=True):
+        super(UNetPlusPlus, self).__init__()
+
+        filters = [n1, n1 * 2, n1 * 4, n1 * 8, n1 * 16]
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.Up = nn.ModuleList([
+            nn.Upsample(size=(height // (2 ** c), width // (2 ** c)), mode='bilinear', align_corners=True) 
+            for c in range(4)
+        ])
+        self.supervision = supervision
+
+        self.conv0_0 = ConvBlockNested(in_ch, filters[0], filters[0])
+        self.conv1_0 = ConvBlockNested(filters[0], filters[1], filters[1])
+        self.conv2_0 = ConvBlockNested(filters[1], filters[2], filters[2])
+        self.conv3_0 = ConvBlockNested(filters[2], filters[3], filters[3])
+        self.conv4_0 = ConvBlockNested(filters[3], filters[4], filters[4])
+
+        self.conv0_1 = ConvBlockNested(filters[0] + filters[1], filters[0], filters[0])
+        self.conv1_1 = ConvBlockNested(filters[1] + filters[2], filters[1], filters[1])
+        self.conv2_1 = ConvBlockNested(filters[2] + filters[3], filters[2], filters[2])
+        self.conv3_1 = ConvBlockNested(filters[3] + filters[4], filters[3], filters[3])
+
+        self.conv0_2 = ConvBlockNested(filters[0] * 2 + filters[1], filters[0], filters[0])
+        self.conv1_2 = ConvBlockNested(filters[1] * 2 + filters[2], filters[1], filters[1])
+        self.conv2_2 = ConvBlockNested(filters[2] * 2 + filters[3], filters[2], filters[2])
+
+        self.conv0_3 = ConvBlockNested(filters[0] * 3 + filters[1], filters[0], filters[0])
+        self.conv1_3 = ConvBlockNested(filters[1] * 3 + filters[2], filters[1], filters[1])
+
+        self.conv0_4 = ConvBlockNested(filters[0] * 4 + filters[1], filters[0], filters[0])
+
+        self.seg_outputs = nn.ModuleList([
+            nn.Conv2d(filters[0], out_ch, kernel_size=1, padding=0) for _ in range(4)
+        ])
+
+    def forward(self, x):
+        seg_outputs = []
+
+        x0_0 = self.conv0_0(x)
+        x1_0 = self.conv1_0(self.pool(x0_0))
+        x0_1 = self.conv0_1(torch.cat([x0_0, self.Up[0](x1_0)], 1))
+        seg_outputs.append(self.seg_outputs[0](x0_1))
+
+        x2_0 = self.conv2_0(self.pool(x1_0))
+        x1_1 = self.conv1_1(torch.cat([x1_0, self.Up[1](x2_0)], 1))
+        x0_2 = self.conv0_2(torch.cat([x0_0, x0_1, self.Up[0](x1_1)], 1))
+        seg_outputs.append(self.seg_outputs[1](x0_2))
+
+        x3_0 = self.conv3_0(self.pool(x2_0))
+        x2_1 = self.conv2_1(torch.cat([x2_0, self.Up[2](x3_0)], 1))
+        x1_2 = self.conv1_2(torch.cat([x1_0, x1_1, self.Up[1](x2_1)], 1))
+        x0_3 = self.conv0_3(torch.cat([x0_0, x0_1, x0_2, self.Up[0](x1_2)], 1))
+        seg_outputs.append(self.seg_outputs[2](x0_3))
+
+        x4_0 = self.conv4_0(self.pool(x3_0))
+        x3_1 = self.conv3_1(torch.cat([x3_0, self.Up[3](x4_0)], 1))
+        x2_2 = self.conv2_2(torch.cat([x2_0, x2_1, self.Up[2](x3_1)], 1))
+        x1_3 = self.conv1_3(torch.cat([x1_0, x1_1, x1_2, self.Up[1](x2_2)], 1))
+        x0_4 = self.conv0_4(torch.cat([x0_0, x0_1, x0_2, x0_3, self.Up[0](x1_3)], 1))
+        seg_outputs.append(self.seg_outputs[3](x0_4))
+
+        if self.supervision:
+            return seg_outputs
+        else:
+            return seg_outputs[-1]
