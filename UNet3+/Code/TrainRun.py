36b3829fe3e8d95dd0acb174d35134fcad92f2bf
MCG <000914m@gmail.com>
2024-11-25 15:18:31 +0900

    fix: train scheduler bug fix UNet3+ #25

--- UNet3+/Code/TrainRun.py
+++ UNet3+/Code/TrainRun.py
@@ -9,7 +9,7 @@ import torch.optim as optim
 from torch.utils.data import DataLoader
 
 
-from Model.resnetModel import UNet_3Plus_DeepSup
+from Model.HRnetModel import UNet3PlusHRNet
 from DataSet.DataLoder import get_image_label_paths
 from config import IMAGE_ROOT,LABEL_ROOT,BATCH_SIZE,IMSIZE,CLASSES,MILESTONES,NUM_EPOCHS, GAMMA,LR, SAVED_DIR
 from DataSet.Dataset import XRayDataset
@@ -88,7 +88,7 @@ def main():
         drop_last=False,
     )
 
-    model = UNet_3Plus_DeepSup(n_classes=len(CLASSES))
+    model = UNet3PlusHRNet(n_classes=len(CLASSES))
 
     # Loss function 정의
     criterion = CombinedLoss(focal_weight=1, iou_weight=1, ms_ssim_weight=1, dice_weight=0)
