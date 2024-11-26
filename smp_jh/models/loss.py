472359ac4674c6ba9a13a4296fadd9d5d80e2eb2
jung0228 <junghw3333@gmail.com>
2024-11-26 17:03:12 +0900

    feat: hybrid Loss

--- smp_jh/models/loss.py
+++ smp_jh/models/loss.py
@@ -11,6 +11,7 @@ class Loss:
                 - "bce": Binary Cross Entropy
                 - "dice": Dice Loss
                 - "focal": Focal Loss
+                - "iou": IoU Loss
         """
         if loss_type == "bce":
             return nn.BCEWithLogitsLoss()
@@ -18,6 +19,11 @@ class Loss:
             return DiceLoss()
         elif loss_type == "focal":
             return FocalLoss()
+        elif loss_type == "iou":
+            return IoULoss()
+        elif loss_type == "hybrid":
+            print("Hybrid Loss Training")
+            return HybridLoss()
         else:
             raise NotImplementedError(f"Loss type {loss_type} not implemented")
 
@@ -31,3 +37,27 @@ class FocalLoss(nn.Module):
     def __init__(self):
         super().__init__()
         # Focal Loss 구현
+
+class IoULoss(nn.Module):
+    def __init__(self, smooth=1e-6):
+        super().__init__()
+        self.smooth = smooth
+    
+    def forward(self, pred, target):
+        intersection = (pred * target).sum(dim=(2, 3))
+        union = pred.sum(dim=(2, 3)) + target.sum(dim=(2, 3)) - intersection
+        iou = (intersection + self.smooth) / (union + self.smooth)
+        return 1 - iou.mean()
+
+class HybridLoss(nn.Module):
+    def __init__(self, alpha=0.5):
+        super().__init__()
+        self.bce_loss = nn.BCEWithLogitsLoss()
+        self.iou_loss = IoULoss()
+        self.alpha = alpha  # BCE와 IoU loss의 가중치 비율
+    
+    def forward(self, pred, target):
+        bce = self.bce_loss(pred, target)
+        iou = self.iou_loss(pred, target)
+        return self.alpha * bce + (1 - self.alpha) * iou, bce, iou
+
